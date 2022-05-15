901cfbb9f53efabb67902ab41c0365f9ee923b9a
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-15 20:56:20 +0530

    Add files via upload

---
+++ int_float_work.py
@@ -0,0 +1,28 @@
+# a=4 ; b=6.5
+# print(type(a)) ; print(type(b))
+# print(a+2) # 6
+# print(a+b) # 10.5
+# print(b+2.3) # 8.8
+# print(b+1) # 7.5
+
+# r='Realme' ; m=12 ; v=4.3
+# print(type(r)) #  str
+# print(type(m)) #  int
+# print(r+m) # TypeError
+# print(r+v) # TypeError
+
+# Python ---> 5 times
+# print('Python')
+# print('Python')
+# print('Python')
+# print('Python')
+# print('Python')
+# print('Python')
+
+
+# print('Python ' * 10)
+# print(3 * 'hello ')
+
+# print('hi' * 2.4) # TypeError: can't multiply
+
+# print('hello' * 'hi') # TypeError: can't multiply

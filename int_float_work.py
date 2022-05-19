048e93d00bbd2c97a87a14ab30dac154fc71f6cf
thrivediraju <104252421+thrivediraju@users.noreply.github.com>
2022-05-19 16:04:23 +0530

    Update int_float_work.py

--- int_float_work.py
+++ int_float_work.py
@@ -1,9 +1,9 @@
 # a=4 ; b=6.5
 # print(type(a)) ; print(type(b))
-# print(a+2) # 6
-# print(a+b) # 10.5
+# print(a+2)   #  6
+# print(a+b)   # 10.5
 # print(b+2.3) # 8.8
-# print(b+1) # 7.5
+# print(b+1)   # 7.5
 
 # r='Realme' ; m=12 ; v=4.3
 # print(type(r)) #  str
